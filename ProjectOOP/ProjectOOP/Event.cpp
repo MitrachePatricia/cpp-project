b37adedb405aeb81f704b78a9fd66b4004f335cc
Mitrache Patricia-Andreea <mitrachepatricia22@stud.ase.ro>
2023-11-26 19:56:14 +0200

    Finished event.h and Event.cpp.

--- ProjectOOP/ProjectOOP/Event.cpp
+++ ProjectOOP/ProjectOOP/Event.cpp
@@ -1,5 +1,8 @@
 #include "Event.h"
+#include <string>
 
+
+int Event::NO_RUNNING_EVENTS = 0;
 
 void Event::setEventName(const char* newEventName) {
 	if (newEventName[0] < 'A' || newEventName[0]>'Z')
@@ -15,8 +18,7 @@ void Event::setDate(const char* newDate) {
 	if (newDate[2] != '/' || newDate[5] != '/') {
 		throw exception("The data format is wrong");
 	}
-
-	memcpy(this->date, newDate,9);
+	strcpy_s(this->date, newDate);
 }
 
 void Event::setStartingHour(const char* newStartingHour) {
@@ -26,6 +28,12 @@ void Event::setStartingHour(const char* newStartingHour) {
 	if (newStartingHour[2] != ':') {
 		throw exception("The hour format is wrong");
 	}
+	if (newStartingHour[0] > 2||newStartingHour[1]<0)
+		throw exception("Wrong hour introduced");
+	if (newStartingHour[0] == 2 && newStartingHour[1] > 4)
+		throw exception("Wrong hour introduced");
+	if (newStartingHour[3] > 5 || newStartingHour[3] < 0)
+		throw exception("Wrong hour introduced");
 
 	strcpy_s(this->startingHour, newStartingHour);
 }
@@ -36,6 +44,10 @@ void Event::setDuration(int newDuration) {
 	this->duration = newDuration;
 }
 
+void Event::setGendreType(GendreType newGendre){
+	this->gendre = newGendre;
+}
+
 string Event::getEventName() {
 	if (this->eventName != nullptr)
 		return string(this->eventName);
@@ -43,6 +55,7 @@ string Event::getEventName() {
 		return "";
 }
 char* Event::getDate() {
+
 	return Util::copyString(this->date);
 }
 
@@ -52,5 +65,101 @@ char* Event::getStartingHour() {
 
 int Event::getDuration() {
 	return this->duration;
+}
 
+GendreType Event::getGendreType() {
+	return this->gendre;
+}
+
+string Event::gendreTypeToString(GendreType gendre) {
+	switch (gendre) {
+	case GendreType::ROMANCE:
+		return "Romance";
+	case GendreType::DRAMA:
+		return "Drama";
+	case GendreType::HORROR:
+		return "Horror";
+	case GendreType::THRILLER:
+		return"Thriller";
+	case GendreType::COMEDY:
+		return "Comedy";
+	case GendreType::ANIMATED:
+		return"Animated";
+	default:
+		throw exception("There exists no such gendre.");
+	}
+
+
+}
+
+
+void operator<<(ostream& console, Event& event) {
+	console << "******************";
+	console << "\n Event Name: " << event.getEventName();
+	console << "\n The date of the event is: " << event.getEventName();
+	console << "\n The starting hour of the event is: " << event.getStartingHour();
+	console << "\n The duration of the event is: " << event.getDuration();
+	console << "\n The gendre of the event is: " << event.getGendreType();
+
+}
+
+void operator>>(istream& console, Event& event) {
+	cout << "Insert the desired event name: ";
+	char* newEvent;
+	console >> newEvent;
+	event.setEventName(newEvent);
+
+	cout << "Insert desired date for the event:";
+	char* newDate;
+	console >> newDate;
+	event.setDate(newDate);
+
+	cout << "Insert the duration:";
+	int newDuration;
+	console >> newDuration;
+	event.setDuration(newDuration);
+
+	//cout << "Insert the gendre";
+	//string newGendre;
+	//console >> newGendre;  //i dont know how to do this im sorry
+}
+
+Event::Event() : eventName("No name") {
+	this->setDate("01/01/2020");
+	this->setDuration(92);
+	this->setStartingHour("14:30");
+	NO_RUNNING_EVENTS++;
+}
+
+Event::Event(const char* eventName, char* date, char* startingHour, int duration) : eventName(eventName), duration(duration) {
+	this->setDate(date);
+	this->setStartingHour(startingHour);
+	NO_RUNNING_EVENTS++;
+}
+
+Event::~Event() {
+	NO_RUNNING_EVENTS--;
+}
+
+Event Event::operator=(const Event& OtherEvent) {
+	this->setDate(OtherEvent.date);
+	this->setDuration(OtherEvent.duration);
+	this->setStartingHour(OtherEvent.startingHour);
+	return *this;
+}
+
+Event Event::operator+(int value) {
+	Event updatedDuration = *this;
+	updatedDuration.duration += value;
+	return updatedDuration;
+}
+
+bool Event::operator>=(const Event event) {
+	char copy[2];
+	strcpy_s(copy, 2, event.startingHour);
+	copy[2] = NULL;
+	if (copy[0] >= 1 && copy[2] >= 4)
+		return true;
+	else
+		return false;
 }
--- ProjectOOP/ProjectOOP/Event.h
+++ ProjectOOP/ProjectOOP/Event.h
@@ -9,13 +9,15 @@ enum GendreType { ROMANCE = 10, DRAMA, HORROR, THRILLER, COMEDY, ANIMATED };
 class Event {
 private:
 	const char* eventName = nullptr;
-	const char* date[11];      // dd/mm/yyy
+	char date[11];      // dd/mm/yyy
 	char startingHour[6];       // hh:mm
 	int duration = 0;       // in min
 	GendreType gendre;
-	Location* locationName;
-	Seat* seatNumber;
-	Seat* row;
+	//Location* locationName;
+	//Seat* seatNumber;
+	//Seat* row;
+
+	static int NO_RUNNING_EVENTS;
 
 public:
 
@@ -25,6 +27,7 @@ public:
 	void setDate(const char* newDate);
 	void setStartingHour(const char* newStartingHour);
 	void setDuration(int newDuration);
+	void setGendreType(GendreType gendre);
 
 	//getters
 
@@ -33,21 +36,15 @@ public:
 	char* getStartingHour();
 	int getDuration();
 	GendreType getGendreType();
+	static string gendreTypeToString(GendreType gendre);
 
 	// default constructor
 
-	Event() : eventName("No name") {
-		this->setDate("01/01/2020");
-		this->setDuration(92);
-		this->setStartingHour("14:30");
-	}
+	Event();
 
 	//constructor with parameters
 
-	Event(const char* eventName, char* date, char* startingHour, int duration) : eventName(eventName), duration(duration) {
-		this->setDate(date);
-		this->setStartingHour(startingHour);
-	}
+	Event(const char* eventName, char* date, char* startingHour, int duration);
 
 	//destructor
 
@@ -57,11 +54,16 @@ public:
 
 	//copy operator
 
-	Event operator=(const Event& newEvent);
+	Event operator=(const Event& OtherEvent);
 
-	//
+	//operators
+
+	Event operator+(int value);
+	bool operator>=(const Event& event);
 
 	//stream operators
 
+	friend void operator<<(ostream& console, Event& newEvent);
+	friend void operator>>(istream& console, Event& newEvent);
 
 };
